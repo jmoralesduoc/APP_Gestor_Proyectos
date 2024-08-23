9fdbbd045d9dc3c192755970e97ade93801ff20a
Javier Morales <jamoralese@duocuc.cl>
2024-08-22 21:58:42 -0400

    actualización

--- APP_Gestor_Proyecto/src/app/login/login.page.ts
+++ APP_Gestor_Proyecto/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  user={
+    usuario:"",
+    password:""
+  }
+
+  constructor(private router: Router) { } //Intanciar el Router
 
   ngOnInit() {
   }
 
+
+  
+
+
+
 }
+
