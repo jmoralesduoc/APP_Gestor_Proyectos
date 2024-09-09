201fe1aa9fa9f0d133b558ea55913bdd54f2e8df
Javier Morales <jamoralese@duocuc.cl>
2024-09-09 20:13:05 -0300

    cambios

--- APP_Gestor_Proyecto/capacitor.config.ts
+++ APP_Gestor_Proyecto/capacitor.config.ts
@@ -1,12 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
-
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'APP_Gestor_Proyecto',
-  webDir: 'www',
-  "bundledWebRuntime": false,
-  "npmClient": "npm"
+  webDir: 'www'
 };
 
 
